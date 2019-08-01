e0f5d00d0191fac5c6420eb3cc3cdac23f42b6cf
Anna <46939030+EsterHani@users.noreply.github.com>
2019-08-01 22:16:13 +0300

    Add files via upload

--- My_game/index.js
+++ My_game/index.js
@@ -1,10 +1,13 @@
 let answer;
 let score = 0;
+let dissipation = 3;
 
 startGame();
 
 document.getElementById("container").addEventListener("click", function(Event) {
-    if (!Event.target.className.match(/square/)){return;}
+    if (!Event.target.className.match(/square/)) {
+        return;
+    }
     if (Event.target.textContent == answer) {
         startGame();
     } else {
@@ -18,27 +21,27 @@ function startGame() {
     document.getElementById("first").textContent = first;
     document.getElementById("second").textContent = second;
     answer = first * second;
-    
+
     let squares = document.querySelectorAll(".square");
-    
+
     let numbers = [];
-    for (let i = 0; i < squares.length - 1; i++) {
-        let x = getRandomInt(1, 100);
-        while (~numbers.indexOf(x) || x === answer) {//Проверка, чтобы не было повторных ответов
-            x = getRandomInt(0, 100);
-        }
-        numbers.push(x);
-    }
-    
     numbers.push(answer);
+    while (numbers.length < squares.length) {
+        let x = getRandomInt(answer - dissipation, answer + dissipation);
+        if (x > 0) {
+            if (numbers.indexOf(x) == -1) {
+                numbers.push(x);
+            }
+        }
+    }
+
     function sortfunction(a, b) {
         return (Math.random() - 0.5);
     }
     numbers.sort(sortfunction);
-    
-    
+
     for (let i = 0; i < squares.length; i++) {
-    squares[i].textContent = numbers[i];
+        squares[i].textContent = numbers[i];
     }
     document.getElementById("account").textContent = score++;
 }
@@ -49,17 +52,17 @@ function getRandomInt(min, max) {
     return rand;
 }
 
-function windowNewGame(){
+function windowNewGame() {
     let show = document.getElementById("show");
     let showClose = document.querySelector(".close");
     let accountEnd = document.querySelector(".accountEnd");
-    show.style.opacity="1";
-    show.style.zIndex="2";
-    document.getElementById("accountEnd").textContent = score-1;
-    showClose.onclick=function(){
-        show.style.opacity="0";
-        show.style.zIndex="0";
+    show.style.opacity = "1";
+    show.style.zIndex = "2";
+    document.getElementById("accountEnd").textContent = score - 1;
+    showClose.onclick = function() {
+        show.style.opacity = "0";
+        show.style.zIndex = "0";
         score = 0;
-        startGame();   
+        startGame();
     }
 }
